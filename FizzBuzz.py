4db56bb243b366f406e784aba0c88a1fadb26ae9
z c <zjcch7@umsystem.edu>
2023-03-09 13:38:01 -0600

    added FizzBuzz.py

---
+++ FizzBuzz.py
@@ -0,0 +1,21 @@
+count = 0
+while (count <= 99):
+
+
+        count += 1
+        
+        if count%3 == 0:
+            
+            print('Fizz')
+
+        elif count%5 == 0:
+
+            print('Buzz')
+
+        else:
+
+            print(count)
+
+
+        
+    
